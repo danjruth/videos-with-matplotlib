be1d472fe11d32a15771c2dbf388fd7bc77fdf2d
Dan Ruth <danjruth@gmail.com>
2020-01-06 12:23:57 -0500

    Create spin.py

---
+++ video_creator/spin.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 12:20:39 2020
+
+@author: ldeike
+"""
+
+class Spin:
+    
+    def __init__(self,fig,ax,period=3,fps=30,):
+        pass
+    
+    def write_video(self,fpath):
+        pass
